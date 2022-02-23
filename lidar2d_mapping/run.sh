29b7ae43c44112c3f24723e80383a269fa3c913e
Ubuntu <ubuntu@ip-172-31-42-202.ap-northeast-2.compute.internal>
2022-02-23 01:24:47 +0000

    slam_example upload

--- lidar2d_mapping/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-docker run --net=host -it --rm \
-            -v $(realpath ..):/root/catkin_ws/src/SLAM-example/lidar2d_mapping \
-            -w /root/catkin_ws/src/SLAM-example/lidar2d_mapping \
-            $@ \
-            lidar2d_mapping
--- slam3d/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-docker run --net=host -it --rm \
-            -v $(realpath ..):/root/catkin_ws/src/SLAM-example/SC-LeGO-LOAM/LeGO-LOAM \
-            -w /root/catkin_ws/src/SLAM-example/SC-LeGO-LOAM/LeGO-LOAM \
-            $@ \
-            lego_loam
